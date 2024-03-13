764ca456263e81d9c303363ee77fc89c77971d26
806-17 <806-17@DESKTOP-ULMFMBS>
2024-03-13 16:14:14 +0900

    ㅡㅐㄱㄷ

--- JavaEx/src/com/javaex/basic/types/ConstantEx.java
+++ JavaEx/src/com/javaex/basic/types/ConstantEx.java
@@ -10,6 +10,10 @@ public class ConstantEx {
 		System.out.println(SPEED_LIMIT);
 		
 		System.out.println("현재 제한 속도는 " + SPEED_LIMIT + "km입니다.");
+		
+		// 상수의 사용 목적
+		// 1. 코드 가독성을 높인다.
+		// 2. 특정 데이터를 한 곳에서 일괄 관리한다. 
 	}
 
 }
