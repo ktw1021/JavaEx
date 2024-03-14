cd10d6a81a879ec1d5df5a9399e72ab81cf01c7c
806-17 <806-17@DESKTOP-ULMFMBS>
2024-03-14 16:43:20 +0900

    3일차 중간

--- JavaEx/src/com/javaex/basic/console/ConsoleInputEx.java
+++ JavaEx/src/com/javaex/basic/console/ConsoleInputEx.java
@@ -17,7 +17,15 @@ public class ConsoleInputEx {
 		System.out.print("이름은?:");
 		name = scanner.next();
 		
-		System.out.print("안녕하세요, "+name + "님:");
+		System.out.print("안녕하세요, "+name + "님. " + age + "살이시네요. 반갑습니다.");
+		
+		if (age<23) {
+			System.out.println("젊으시네요.");
+		}
+		else {
+			System.out.println("틀딱이네.");
+		}
+		
 		
 		scanner.close();
 		
